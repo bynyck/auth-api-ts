383641545ce599861e1a8ff649a420325f34352c
bynyck <nicollasnascimento.pc@gmail.com>
2026-08-18 09:54:39 -0300

    chore: configura express

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import createApp from "./app.js";
+
+const app = createApp();
+
+const port = Number(process.env.PORT) || 8080;
+
+app.listen(port, () => {
+    console.log("Servidor rodando na porta: " + port);
+})
